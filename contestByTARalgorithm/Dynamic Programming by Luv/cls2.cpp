c90769a87fa74768377edaf8a0d1626507c91e8f
Arman Hossen <145398391+armanhossen-dev@users.noreply.github.com>
2025-04-05 07:42:28 +0600

    cpp

--- contestByTARalgorithm/Dynamic Programming by Luv/cls2.cpp
+++ contestByTARalgorithm/Dynamic Programming by Luv/cls2.cpp
@@ -4,17 +4,20 @@
 using namespace std;
 const int N = 1e5+10;
 
+#define f_loop(i,a,b) for(int i = a; i<=b; i++)
+
 int func(int idx){
  //8.31/22.41
+ //https://youtu.be/mnuBvHbMNJM?si=WjXOsq7XXdgJGlc6
 }
 
 //https://youtu.be/mnuBvHbMNJM?si=KOD_4ODWRm8QwE_6
-
+//https://sketch.io/sketchpad/
 
 
 int main(){
-    //cout << "Hello Arman" << endl;
-
-
+    f_loop(i,1,5){
+        cout << i <<". Hello Arman" << endl;
+    }
     return 0;
 }
